f7309b34238b0677544d02b3394ef9415c7b7126
Ankita444-Creator <nandiankita34@gmail.com>
2026-07-12 15:12:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -58,3 +58,20 @@ function loadRestaurants() {
     });
 }
 loadRestaurants();
+const restaurants = [
+    {
+        name: "The Wok Mama",
+        phone: "07718296610"
+    }
+];
+
+const container = document.getElementById('restaurant-container');
+
+restaurants.forEach(r => {
+    container.innerHTML += `
+        <div class="card">
+            <h2>${r.name}</h2>
+            <p><strong>Contact:</strong> ${r.phone}</p>
+        </div>
+    `;
+});
